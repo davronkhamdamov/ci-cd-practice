49cda1361e6e0418967afaa87187f244a7262a85
davronkhandamov <xamdamovdavron6@gmail.com>
2023-12-28 05:13:47 +0500

    ad

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-app.use(cors())
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello")
-})
+    res.send("Hello world");
+});
 
-
-app.listen(3000)
+app.listen(3000);
